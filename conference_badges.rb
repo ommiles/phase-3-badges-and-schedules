ef16880bdd80f4d8da35f7946fde9255d46f4ef8
OM Miles <miles@ommiles.com>
2021-05-17 10:42:06 -0700

    commit tree 02907e68397d17c688acb28e5d84d70190db1cf9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badge_messages = []
+    attendees.each{|name| badge_messages.push("Hello, my name is #{name}.")}
+    badge_messages
+end
+
+def assign_rooms(speakers)
+    room_assignments = []
+    speakers.each_with_index {|name, index| room_assignments.push ("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+    room_assignments
+end
+
+def printer(humans)
+    batch_badge_creator(humans).each {|name| puts name}
+    assign_rooms(humans).each {|name| puts name}
+end
